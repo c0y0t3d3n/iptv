8d2c73fb4f00990ad5597c8b54b20e41b92da2bb
Coyote <coyoteden@icloud.com>
2026-07-24 19:00:23 -0400

    Add files via upload
    add util to fetch url/user/pass from AMZ via hash.

---
+++ amz.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+import requests
+import json
+
+REGISTER_URL='https://iptv.tutoje.cz/api/import-key/register'
+FETCH_URL='https://iptv.tutoje.cz/api/import'
+APIKEY='.amz_api_key'
+h = sys.argv[1]
+try:
+    with open(APIKEY) as f:
+        api_key=f.read()
+except:
+    api_key=None
+
+if not api_key:
+    try:
+        r=requests.post(REGISTER_URL)
+        r.raise_for_status()
+        api_key=json.loads(r.text)['api_key']
+        with open(APIKEY,'w') as f:
+            f.write(api_key)
+    except Exeption as e:
+        print(e,r,file=sys.stderr)
+        api_key=None
+
+if api_key:
+    try:
+        r=requests.get(FETCH_URL+'/'+h,headers={'X-Api-Key':api_key})
+        r.raise_for_status()
+        j=json.loads(r.text)
+        print ('%(server)s %(username)s %(password)s'%j)
+    except Exception as e:
+        print (e,r,file=sys.stderr)
+
+        
